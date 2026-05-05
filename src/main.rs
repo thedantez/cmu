cc238bfd186ecb934caedf3d98efc4566c7155ba
thedantez <dsemenin1@gmail.com>
2026-05-05 16:06:25 +0700

    workable module vk_api: it send json from vk

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ mod vk_api;
 use vk_api::VkClient;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let token = "vk1.a.1wOvxYOFAFijemO8VNUkDl1SjwPagbbzz71kSvtdNQmimc8GFEZIAwHt1Lgkwdz72gR6rLgpqyalY0UUcAUz6hGU5-8bGFjkMGhCSEIvhx9rvoR1SPuq51Br02lmGE9LDZ25Vxb5GOgRfucpMiRsd2QK2_Iy0shPZfPgwJOEDzSMJGaovLKZ_JmHwJeBvcXpK3xAwPx-iy3I_1P6MF6gjw";
     let client = VkClient::new(token.to_string());
-    client.get_dialogs().await.unwrap();
+    let dialogs = client.get_dialogs().await?;
+    for d in dialogs {
+        println!("{} : {}", d.peer_id, d.title);
+    }
+    Ok(())
 }
--- src/vk_api.rs
+++ src/vk_api.rs
@@ -1,3 +1,5 @@
+//  api calls to api.vk.com
+
 use reqwest;
 use serde_json::Value;
 
@@ -13,23 +15,43 @@ impl VkClient {
             client: reqwest::Client::new(),
         }
     }
-    pub async fn get_dialogs(&self) -> Result<(), Box<dyn std::error::Error>> {
+    pub async fn get_dialogs(&self) -> Result<Vec<Dialog>, Box<dyn std::error::Error>> {
         let url = format!(
             "https://api.vk.com/method/messages.getConversations?access_token={}&v=5.199&count=3",
             self.token
         );
         let resp = self.client.get(&url).send().await?.json::<Value>().await?;
-        println!("answer from vk: {}", resp);
-        Ok(())
+        let items = resp["response"]["items"]
+            .as_array()
+            .ok_or("not found response.items")?;
+        let mut dialogs = Vec::new();
+        for item in items {
+            let peer_id = item["conversation"]["peer"]["id"]
+                .as_i64()
+                .ok_or("not found peer.id")?;
+            let title = if let Some(chat_settings) = item["conversation"]["chat_settings"].as_object() {
+                chat_settings.get("title")
+                    .and_then(|v| v.as_str())
+                    .unwrap_or("w/out title")
+            } else {
+                "myselft chat"
+            };
+            let dialog = Dialog {
+                peer_id,
+                title: title.to_string(),
+            };
+            dialogs.push(dialog);
+        }
+        Ok(dialogs)
     }
 }
 
-struct Dialog {
+pub struct Dialog {
     pub title: String,
     pub peer_id: i64,
 }
 
-struct Message {
+pub struct Message {
     pub sender_name: String,
     pub text: String,
 }
