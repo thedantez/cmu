6ad858ee4d542cdd5d3ed856ab1a8a0f5de11edb
thedantez <dsemenin1@gmail.com>
2026-05-05 15:17:28 +0700

    workable module vk_api: it send json from vk

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 mod vk_api;
+use vk_api::VkClient;
 
 #[tokio::main]
 async fn main() {
-    let client = vk_api::VkClient::new(TOKEN.to_string());
-    match client.get_dialogs().await {
-        Ok(dialogs) => {
-            for d in dialogs {
-                println!("{}: {}\n", d.peer_id, d.title);
-            }
-        }
-        Err(e) => eprintln!("error: {}", e),
-    }
+    let token = "vk1.a.1wOvxYOFAFijemO8VNUkDl1SjwPagbbzz71kSvtdNQmimc8GFEZIAwHt1Lgkwdz72gR6rLgpqyalY0UUcAUz6hGU5-8bGFjkMGhCSEIvhx9rvoR1SPuq51Br02lmGE9LDZ25Vxb5GOgRfucpMiRsd2QK2_Iy0shPZfPgwJOEDzSMJGaovLKZ_JmHwJeBvcXpK3xAwPx-iy3I_1P6MF6gjw";
+    let client = VkClient::new(token.to_string());
+    client.get_dialogs().await.unwrap();
 }
--- src/vk_api.rs
+++ src/vk_api.rs
@@ -1,21 +1,35 @@
 use reqwest;
 use serde_json::Value;
 
-struct VkClient {
-    const TOKEN: &str = "vk1.a.1wOvxYOFAFijemO8VNUkDl1SjwPagbbzz71kSvtdNQmimc8GFEZIAwHt1Lgkwdz72gR6rLgpqyalY0UUcAUz6hGU5-8bGFjkMGhCSEIvhx9rvoR1SPuq51Br02lmGE9LDZ25Vxb5GOgRfucpMiRsd2QK2_Iy0shPZfPgwJOEDzSMJGaovLKZ_JmHwJeBvcXpK3xAwPx-iy3I_1P6MF6gjw";
-    let url = format!(
-        "https://api.vk.com/method/messages.getConversations?access_token={}&v=5.199&count=3",
-        TOKEN
-    );
-    let resp = reqwest::get(&url).await?.json::<Value>().await?;
+pub struct VkClient {
+    token: String,
+    client: reqwest::Client,
+}
+
+impl VkClient {
+    pub fn new(token: String) -> Self {
+        VkClient {
+            token,
+            client: reqwest::Client::new(),
+        }
+    }
+    pub async fn get_dialogs(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let url = format!(
+            "https://api.vk.com/method/messages.getConversations?access_token={}&v=5.199&count=3",
+            self.token
+        );
+        let resp = self.client.get(&url).send().await?.json::<Value>().await?;
+        println!("answer from vk: {}", resp);
+        Ok(())
+    }
 }
 
 struct Dialog {
-    title: &str = "";
-    url: &str = "";
+    pub title: String,
+    pub peer_id: i64,
 }
 
 struct Message {
-    owner: &str = "";
-    text: &str = "";
+    pub sender_name: String,
+    pub text: String,
 }
